5969472bbc26ad0be4343f1ce1d966c412232271
carlos moreno <camorenofro@gmail.com>
2023-10-18 18:57:59 -0500

    Se completa Ejercicio125

--- ejercicio125/Ejercicio125/src/main.cpp
+++ ejercicio125/Ejercicio125/src/main.cpp
@@ -1,35 +1,27 @@
 #include <Arduino.h>
-#include <fstream>
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-
-void lectura();
-void setup() {  
- Serial.begin(115200);
- Serial.println();
-lectura();
+#include "SPIFFS.h"
+ 
+void setup() {
+  Serial.begin(115200);
+  
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  
+  File file = SPIFFS.open("/prueba.txt");
+  if(!file){
+    Serial.println("Problemas al leer el archivo");
+    return;
+  }
+  
+  Serial.println("File Content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
 }
-
-void lectura() {
-
-       std:: ifstream archivo;
-        std:: string texto;
-
-archivo.open("C:\\prueba\\prueba.txt", std::ios::in);
-        if (archivo.fail())
-        {
-          Serial.println("Error al abrir el archivo");
-          exit(1);
-        }
-        while (!archivo.eof())
-        {
-          getline(archivo, texto);
-          Serial.println(String(texto.c_str()));
-        }
-        archivo.close();
-}
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
+}
